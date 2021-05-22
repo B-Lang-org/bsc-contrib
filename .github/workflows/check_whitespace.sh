49fb348764ca8f30a68fba2d884c588b681fc4f9
Julie Schwartz <quark@bluespec.com>
2021-05-22 14:51:52 +1200

    Fix the CI's whitespace check
    The exit code of the if-condition command (xargs) was false if any
    file did not contain the pattern (since grep returns success if found)
    and so the whitespace check was passing even though a file was
    committed with trailing whitespace!

--- .github/workflows/check_whitespace.sh
+++ .github/workflows/check_whitespace.sh
@@ -2,16 +2,30 @@
 
 set -e
 
+RESULT=0
+
+# -----
+# Check for trailing whitespace
+
 # For now, restrict to Bluespec code
-if git ls-files | egrep '\.(bs|bsv)$' | xargs grep -n ' $'; then
-  echo "Trailing whitespace found!"
-  exit 1
+CMD="git ls-files | egrep '\.(bs|bsv)$' | xargs grep -H -n -e ' $'"
+if [ $(eval "$CMD -l -- | wc -l") -ne 0 ]; then
+    eval "$CMD --" || true
+    echo "Trailing whitespace found!"
+    RESULT=1
 fi
+
+# -----
+# Check for tabs
 
 # Don't allow tabs in BH/Classic code, where formatting matters
-if git ls-files | egrep '\.bs$' | xargs grep -n $'\t'; then
-  echo "Tabs found!"
-  exit 1
+CMD="git ls-files | egrep '\.bs$' | xargs grep -H -n -e $'\t'"
+if [ $(eval "$CMD -l -- | wc -l") -ne 0 ]; then
+    eval "$CMD --" || true
+    echo "Tabs found!"
+    RESULT=1
 fi
 
-exit 0
+# -----
+
+exit $RESULT
